d6fff1863d6fecd3b9b8acdf15fd3a62b5384995
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2021-05-01 21:41:29 +0700

    refactor: add email utilities

--- send.py
+++ send.py
@@ -1,6 +1,16 @@
+# Import required libraries
 import csv
 import smtplib
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 from email.mime.base import MIMEBase
 from email import encoders
+
+# Email address of sender
+emailSender = ""
+
+# Password of sender's email address
+emailPassword = ""
+
+# Subject of the email that will be sent
+emailSubject = ""
